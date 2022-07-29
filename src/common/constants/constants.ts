1ccf6cda57001d6ff83171b6fbc75b9388bfac1f
Daniel Gaievskyi <dgaievskyi@gmail.com>
2022-07-29 13:36:33 +0200

    Refactor constants.

--- src/common/constants/constants.ts
+++
@@ -1,1 +0,0 @@
-export const API_URL = 'https://62e02886fa8ed271c47f8258.mockapi.io/'
--- src/common/constants/index.ts
+++ src/common/constants/index.ts
@@ -1,1 +1,1 @@
-export * from './constants'
+export const API_URL = 'https://62e02886fa8ed271c47f8258.mockapi.io/'
