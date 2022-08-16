fb37e2e2dd21e64463e8966231e7f7958e3550a6
Makar Bialiauski <directmac@mail.ru>
2022-08-16 16:04:12 +0200

    Changed create-app(main.py)

--- backend/src/main.py
+++ backend/src/main.py
@@ -3,15 +3,10 @@ from .api.routers import api_router
 from .core.constants import TITLE, VERSION, DOCS_URL
 
 
-def create_app() -> FastAPI:
-    app = FastAPI(
-        docs_url=DOCS_URL,
-        title=TITLE,
-        version=VERSION,
-    )
-    app.include_router(api_router)
-    
-    return app
+app = FastAPI(
+    docs_url=DOCS_URL,
+    title=TITLE,
+    version=VERSION,
+)
 
-
-app = create_app()
+app.include_router(api_router)
