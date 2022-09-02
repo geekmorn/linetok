90fe4cc14ca9d677501b0f744e0e0feb161ccc82
Makar Bialiauski <directmac@mail.ru>
2022-09-02 21:27:08 +0200

    Moved from services

---
+++ backend/src/common/utils/exceptions.py
@@ -0,0 +1,22 @@
+from fastapi import HTTPException
+
+
+def not_found(message: str) -> HTTPException:
+    raise HTTPException(
+        status_code=404,
+        detail=f"{message} not found"
+    )
+
+
+def unauthorized(message="You are not authorized to access this resource") -> HTTPException:
+    raise HTTPException(
+        status_code=401,
+        detail=message
+    )
+
+
+def conflict(message: str) -> HTTPException:
+    raise HTTPException(
+        status_code=409,
+        detail=message
+    )
