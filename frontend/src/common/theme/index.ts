d39190cf193c4ca05b4dee9732ecf945a2b381fc
Daniel Gaievskyi <dgaievskyi@gmail.com>
2022-10-23 14:51:38 +0200

    Add fonts

--- frontend/src/common/theme/index.ts
+++ frontend/src/common/theme/index.ts
@@ -2,13 +2,13 @@ import { Button, Skeleton, TableContainer } from 'common/theme/components'
 import { extendTheme } from '@chakra-ui/react'
 
 export const theme = extendTheme({
-  fonts: {
-    heading: `'Jost', sans-serif`,
-    body: `'Jost', sans-serif`,
-  },
   components: {
     Button,
     Skeleton,
     TableContainer
+  },
+  fonts: {
+    body: `'Jost', sans-serif`,
+    heading: `'Jost', sans-serif`
   }
 })
--- frontend/src/modules/products/hooks/use-read-product.query.ts
+++ frontend/src/modules/products/hooks/use-read-product.query.ts
@@ -3,7 +3,7 @@ import { ProductType } from 'common/types'
 import { read } from 'common/utils'
 import { useQuery } from '@tanstack/react-query'
 
-export const useReadProductByIdQuery = (id: ProductType["id"]) =>
+export const useReadProductByIdQuery = (id: ProductType['id']) =>
   useQuery(
     ['Read product by ID', id],
     async () => await read<ProductType>(API.products, id)
